f74600376583a18145bd6691c45af421a0b71dad
unknown <senanaal@code.edu.az>
2024-02-28 11:52:52 +0400

    LAB

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 0.7;
+        double epsilon = 0.01;
+        double sum = 0;
+        double term;
+        int n = 1;
+
+        do
+        {
+            term = Math.Pow(-1, n - 1) * Math.Sin(Math.Pow(x, n) / n);
+            sum += term;
+            n++;
+        } while (Math.Abs(term) >= epsilon);
+
+        Console.WriteLine("Sum of the series: " + sum);
+    }
+}
